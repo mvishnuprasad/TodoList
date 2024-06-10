6cc4f76ec3392e680329361052d2fdad015fa0b0
vishnuprasad <vishnup@yeswearemad.com>
2024-06-10 11:13:41 +0530

    initial commit : Homepage

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'pages/TaskScreen.dart';
+
+void main() {
+  runApp(const ToDolist());
+}
+class ToDolist extends StatelessWidget {
+  const ToDolist({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: TaskScreen(),
+    );
+  }
+}
---
+++ lib/pages/TaskScreen.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+
+class TaskScreen extends StatelessWidget {
+  const TaskScreen({super.key});
+
+  //const ({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: Colors.lightBlueAccent,
+        floatingActionButton: FloatingActionButton(
+          backgroundColor: Colors.lightBlueAccent,
+          child: const Icon(
+            Icons.add,
+            color: Colors.white,
+          ),
+          onPressed: () {},
+        ),
+        body: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(
+              padding: const EdgeInsets.only(
+                  top: 60, left: 30, right: 30, bottom: 30),
+              child: const Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  CircleAvatar(
+                    backgroundColor: Colors.white,
+                    radius: 30,
+                    child: Icon(
+                      Icons.list,
+                      size: 30,
+                      color: Colors.lightBlueAccent,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Text(
+                    "TodoList",
+                    style: TextStyle(
+                        fontSize: 50,
+                        fontWeight: FontWeight.w800,
+                        color: Colors.white),
+                  ),
+                  Text(
+                    "13 tasks",
+                    style: TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.w400,
+                        color: Colors.white),
+                  ),
+                ],
+              ),
+            ),
+            Expanded(
+              child: Container(
+                decoration: const BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(20),
+                        topRight: Radius.circular(20))),
+              ),
+            )
+          ],
+        ));
+  }
+}
